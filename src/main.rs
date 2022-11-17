df6f99268eac4f7484debb1df721ca83b9ea7d87
victrolaface <rlovelett@gmail.com>
2022-11-17 17:00:15 -0600

    updated cargo, vscode config

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+//use bevy::prelude::*;
+
+fn main() {
+    println!("hello world");
+    let a: i32 = 10;
+    println!("{a}");
+    //App::new()
+    //    .add_plugins(DefaultPlugins)
+    //    .run();
+}
